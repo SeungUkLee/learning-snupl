01e04040153491f3ae3344e4ff3fb3585e32d344
SeungukLee <seunguk@seunguk.dev>
2023-02-01 12:49:05 +0900

    implement simple type checker

--- simple/simple_checker.ml
+++ simple/simple_checker.ml
@@ -31,8 +31,8 @@ let rec typ_to_string = function
   | TString -> "string"
   | TPair (t1, t2) -> "pair(" ^ typ_to_string t1 ^ ", " ^ typ_to_string t2 ^ ")"
   | TLoc l -> "loc (" ^ typ_to_string l ^ ")"
-  | TFun (t1, t2) -> "fun (" ^ typ_to_string t1 ^ ") -> " ^ typ_to_string t2
-  | TVar _ -> "type var"
+  | TFun (t1, t2) -> "(" ^ typ_to_string t1 ^ " -> " ^ typ_to_string t2 ^ ")"
+  | TVar x -> x
 
 module TEnv = struct
   type t = M.id -> typ
@@ -43,12 +43,22 @@ module TEnv = struct
 end
 
 module Subst = struct
-  type t = (tyvar * typ) list
+  type t = typ -> typ
 
-  let empty = []
-  let make (x, t) = [ (x, t) ]
-  let compose : t -> t -> t = fun s s' -> s @ s'
-  let find subst x = List.assoc x subst
+  let empty : t = fun s -> s
+  let compose s' s ty = s' (s ty)
+
+  let make ((x : tyvar), (t : typ)) =
+    let rec subst (ty : typ) : typ =
+      match ty with
+      | TInt | TBool | TString -> ty
+      | TPair (t1, t2) -> TPair (subst t1, subst t2)
+      | TLoc t -> TLoc (subst t)
+      | TFun (tfunc, tparam) -> TFun (subst tfunc, subst tparam)
+      | TVar name when not (x = name) -> ty
+      | TVar name -> t
+    in
+    subst
 
   let rec apply : t -> typ -> typ =
    fun subst typ ->
@@ -58,11 +68,8 @@ module Subst = struct
     | TString -> TString
     | TPair (a, b) -> TPair (apply subst a, apply subst b)
     | TLoc l -> TLoc (apply subst l)
-    | TVar x -> ( try find subst x with _ -> typ)
+    | TVar x -> subst typ
     | TFun (t1, t2) -> TFun (apply subst t1, apply subst t2)
-
-  let extend subst (x, t) =
-    (x, t) :: List.map (fun (x, t) -> (x, apply [ (x, t) ] t)) subst
 
   let apply_tenv : t -> TEnv.t -> TEnv.t =
    fun subst tenv x -> apply subst (TEnv.find tenv x)
@@ -81,10 +88,11 @@ let rec unify : typ * typ -> Subst.t =
     | a, b when a = b -> Subst.empty
     | TVar a, t when not (occurs a t) -> Subst.make (a, t)
     | t, TVar a when not (occurs a t) -> Subst.make (a, t)
-    | TFun (t1, t2), TFun (t1', t2') ->
+    | TPair (t1, t2), TPair (t1', t2') | TFun (t1, t2), TFun (t1', t2') ->
         let s = unify (t1, t1') in
         let s' = unify (Subst.apply s t2, Subst.apply s t2') in
-        Subst.compose s s'
+        Subst.compose s' s
+    | TLoc t1, TLoc t2 -> unify (t1, t2)
     | _ ->
         raise
           (M.TypeError
@@ -97,48 +105,56 @@ let rec m_algorithm : TEnv.t -> M.exp -> typ -> Subst.t =
   | M.CONST (N _) -> unify (TInt, expected)
   | M.CONST (B _) -> unify (TBool, expected)
   | M.CONST (S _) -> unify (TString, expected)
-  | M.VAR x -> unify (expected, TEnv.find tenv x)
+  | M.VAR x -> unify (TEnv.find tenv x, expected)
   | M.FN (x, e) ->
       let a1, a2 = (TVar (new_var ()), TVar (new_var ())) in
       let s = unify (TFun (a1, a2), expected) in
-      let tenv = TEnv.extend tenv (x, Subst.apply s a1) in
-      let s' = m_algorithm (Subst.apply_tenv s tenv) e (Subst.apply s a2) in
+      let tenv, a2 =
+        ( TEnv.extend (Subst.apply_tenv s tenv) (x, Subst.apply s a1),
+          Subst.apply s a2 )
+      in
+      let s' = m_algorithm tenv e a2 in
       Subst.compose s' s
   | M.APP (e1, e2) ->
       let a = TVar (new_var ()) in
       let s = m_algorithm tenv e1 (TFun (a, expected)) in
-      let s' = m_algorithm (Subst.apply_tenv s tenv) e2 (Subst.apply s a) in
+      let tenv, a = (Subst.apply_tenv s tenv, Subst.apply s a) in
+      let s' = m_algorithm tenv e2 a in
       Subst.compose s' s
   | M.LET (M.VAL (x, e1), e2) ->
       let a = TVar (new_var ()) in
       let s = m_algorithm tenv e1 a in
-      let tenv = TEnv.extend tenv (x, Subst.apply s a) in
-      let s' =
-        m_algorithm (Subst.apply_tenv s tenv) e2 (Subst.apply s expected)
+      let tenv, expected =
+        ( TEnv.extend (Subst.apply_tenv s tenv) (x, Subst.apply s a),
+          Subst.apply s expected )
       in
+      let s' = m_algorithm tenv e2 expected in
       Subst.compose s' s
   | M.LET (M.REC (f, x, e1), e2) ->
       let a = TVar (new_var ()) in
-      let s = m_algorithm tenv (M.FN (x, e1)) a in
-      let tenv = TEnv.extend tenv (f, Subst.apply s a) in
-      let s' =
-        m_algorithm (Subst.apply_tenv s tenv) e2 (Subst.apply s expected)
+      let s = m_algorithm (TEnv.extend tenv (f, a)) (M.FN (x, e1)) a in
+      let tenv, expected, a =
+        (Subst.apply_tenv s tenv, Subst.apply s expected, Subst.apply s a)
       in
+      let s' = m_algorithm (TEnv.extend tenv (f, a)) e2 expected in
       Subst.compose s' s
   | M.IF (e1, e2, e3) ->
       let s = m_algorithm tenv e1 TBool in
-      let s' =
-        m_algorithm (Subst.apply_tenv s tenv) e2 (Subst.apply s expected)
+      let tenv, expected = (Subst.apply_tenv s tenv, Subst.apply s expected) in
+      let s' = m_algorithm tenv e2 expected in
+      let tenv, expected =
+        (Subst.apply_tenv s' tenv, Subst.apply s' expected)
       in
-      let s'' =
-        m_algorithm (Subst.apply_tenv s' tenv) e2 (Subst.apply s' expected)
-      in
+      let s'' = m_algorithm tenv e3 expected in
+
       Subst.compose s'' (Subst.compose s' s)
   | M.BOP (op, e1, e2) -> (
       let bop op r =
         let s = unify (r, expected) in
-        let s' = m_algorithm (Subst.apply_tenv s tenv) e1 op in
-        let s'' = m_algorithm (Subst.apply_tenv s' tenv) e2 op in
+        let tenv, op = (Subst.apply_tenv s tenv, Subst.apply s op) in
+        let s' = m_algorithm tenv e1 op in
+        let tenv, op = (Subst.apply_tenv s' tenv, Subst.apply s' op) in
+        let s'' = m_algorithm tenv e2 op in
         Subst.compose s'' (Subst.compose s' s)
       in
       match op with
@@ -149,34 +165,36 @@ let rec m_algorithm : TEnv.t -> M.exp -> typ -> Subst.t =
   | M.WRITE e ->
       let a = TVar (new_var ()) in
       let s = unify (a, expected) in
-      let s' =
-        m_algorithm (Subst.apply_tenv s tenv) e (Subst.apply s expected)
-      in
+      let tenv, expected = (Subst.apply_tenv s tenv, Subst.apply s expected) in
+      let s' = m_algorithm tenv e expected in
       Subst.compose s' s
   | M.MALLOC e ->
       let a = TVar (new_var ()) in
       let s = m_algorithm tenv e a in
-      let s' = unify (TLoc (Subst.apply s a), Subst.apply s expected) in
+      let expected, a = (Subst.apply s expected, Subst.apply s a) in
+      let s' = unify (TLoc a, expected) in
       Subst.compose s' s
   | M.ASSIGN (e1, e2) ->
       let s = m_algorithm tenv e1 (TLoc expected) in
-      let s' =
-        m_algorithm (Subst.apply_tenv s tenv) e2 (Subst.apply s expected)
-      in
+      let tenv, expected = (Subst.apply_tenv s tenv, Subst.apply s expected) in
+      let s' = m_algorithm tenv e2 expected in
       Subst.compose s' s
   | M.BANG e -> m_algorithm tenv e (TLoc expected)
   | M.SEQ (e1, e2) ->
       let a = TVar (new_var ()) in
       let s = m_algorithm tenv e1 a in
-      let s' =
-        m_algorithm (Subst.apply_tenv s tenv) e2 (Subst.apply s expected)
-      in
+      let tenv, expected = (Subst.apply_tenv s tenv, Subst.apply s expected) in
+      let s' = m_algorithm tenv e2 expected in
       Subst.compose s' s
   | M.PAIR (e1, e2) ->
       let a1, a2 = (TVar (new_var ()), TVar (new_var ())) in
       let s = unify (TPair (a1, a2), expected) in
-      let s' = m_algorithm (Subst.apply_tenv s tenv) e1 (Subst.apply s a1) in
-      let s'' = m_algorithm (Subst.apply_tenv s' tenv) e2 (Subst.apply s' a2) in
+      let tenv, a1, a2 =
+        (Subst.apply_tenv s tenv, Subst.apply s a1, Subst.apply s a2)
+      in
+      let s' = m_algorithm tenv e1 a1 in
+      let tenv, a2 = (Subst.apply_tenv s' tenv, Subst.apply s' a2) in
+      let s'' = m_algorithm tenv e2 a2 in
       Subst.compose s'' (Subst.compose s' s)
   | M.FST e ->
       let a = TVar (new_var ()) in
@@ -192,7 +210,7 @@ let rec convert_typ : typ -> M.types = function
   | TPair (t1, t2) -> M.TyPair (convert_typ t1, convert_typ t2)
   | TLoc l -> M.TyLoc (convert_typ l)
   | TFun (t1, t2) -> M.TyArrow (convert_typ t1, convert_typ t2)
-  | TVar _ -> raise (M.TypeError "convert error")
+  | TVar x -> raise (M.TypeError ("TVar " ^ x ^ " can not be converted."))
 
 let check : M.exp -> M.types =
  fun exp ->
